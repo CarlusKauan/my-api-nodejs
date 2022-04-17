c3d2cc6b37a20b79fc31cde3711c1fed5a052065
carloskauan <carloskauan190@gmail.com>
2022-04-17 19:00:13 -0300

    Api finalizada !

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const routes = require('./router');
+const mongoose = require('mongoose');
+
+// conf inicial
+require('dotenv').config()
 
 // criando a app
 const app = express();
 
 app.use(express.json())
-app.use(routes)
 
-// rodando na porta 3000
-app.listen(3000, () => console.log("🚀"));
+// use rotas
+app.use('/pets',routes)
 
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
+
+mongoose
+    .connect(   
+        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.patyw.mongodb.net/myapinodejs?retryWrites=true&w=majority`
+    )
+    .then(() => {
+        // rodando na porta 3000
+        app.listen(3000, () => console.log("🚀"));
+    })
+    .catch((err) => console.log(err))
--- src/router.js
+++ src/router.js
@@ -1,53 +1,119 @@
+
 const express = require('express');
 const routes = express.Router()
+const Pet = require('../models/Pet');
 
-// banco fake
-let pets = [];
-
-// post {pet}
-routes.post('/pets', (req, res) => {
-    const { id, nome, dono, raca, idade } = req.body;
+routes.post('/', async (req, res) => {
+    
+    // extraindo propriedades
+    const { id, nome, dono, raca, idade } = req.body
+    // object pet
     const pet = { id, nome, dono, raca, idade };
-    pets.push(pet);
-    
-    return res.status(201).json(pet);
-});
+  
+    // validação
+    if(!id){
+        res.status(422).json({message: 'O id é obrigatorio !'})
+        return;
+    }
+    else if(!nome || !dono){
+        res.status(422).json({message: 'Nome ou Dono é obrigatorio !'})
+        return;
+    }
 
-// get {pets}
-routes.get('/pets', (req, res) => { 
-    const buscarPets = pets;
-    
-    return res.status(200).json(buscarPets);
-});
-// get {pets:id}
-routes.get('/pets/:pets_id', (req, res) => { 
-    const { pets_id } = req.params;
-    const pet = pets.find((pet) => pet.id == pets_id);
-    if(!pet) res.status(404).json('não achei patrão !')
-    
-    return res.status(200).json(pet);
-});
+    // create -> try -> catch
+    try {
+        // esperar a req terminar 
+        await Pet.create(pet)
 
-// delete {pet}
-routes.delete('/pets/:pets_id', (req, res) => {
-    const { pets_id } = req.params;
-    const FiltrarPets = pets.filter((pet) => pet.id != pets_id);
-    pets = FiltrarPets;
+        res.status(201).json({message: 'Pet Inserido no sistema com Sucesso !'})
 
-    return res.status(204).json('foi deletado, meu nobre !');
-});
-// patch {pet:id}
-routes.patch('/pets/:pets_id', (req, res) => {
-    const { nome, dono, raca } = req.body;
-    const { pets_id } = req.params;
-    const pet = pets.find(pet => pet.id == pet.id);
-    pet.id = pet.id;
-    pet.nome = nome ? nome : pet.nome;
-    pet.dono = dono ? dono : pet.dono;
-    pet.raca = raca ? raca : pet.raca;
-
-    return res.status(200).json(pet);
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
 })
+
+routes.get('/', async  (req, res) => {
+    // req = enviando uma requisição, res = recebendo uma resposta
+    try {
+        // filtra = find()
+        const pets = await Pet.find()
+
+        res.status(200).json(pets)
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
+})
+
+routes.get('/:id', async (req, res) => { 
+    
+    try {
+        const id = req.params.id
+
+        try {
+            // filtrar um = findOne()
+            const pet = await Pet.findOne({id: id})
+
+            if(!pet){
+                res.status(422).json({message: 'Pet não encontrado !'})
+                return;
+            }
+
+            return res.status(200).json(pet)
+
+        } catch (error) {
+            res.status(500).json({ error: error })
+        }
+
+        return res.status(200).json(buscarPets);
+        
+    } catch (error) {
+        res.status(404).json({message: 'não foi encontrado !'})
+    }
+});
+
+routes.patch('/:id', async (req, res) => {
+    
+    const id = req.params.id
+
+    const { nome, dono, raca, idade} = req.body;
+
+    const pet = { nome, dono, raca, idade };
+
+    try {
+        const UpdatePet = await Pet.updateOne({id: id}, pet)
+
+        if(UpdatePet.matchedCount === 0) {
+            res.status(422).json({message: 'Pet não encontrado !'})
+            return;
+        }
+
+        res.status(200).json(pet)
+
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
+})
+
+routes.delete('/:id', async (req, res) => {
+    
+    const id = req.params.id
+    
+    const pet = await Pet.findOne({id: id})
+
+    if(!pet){
+        res.status(422).json({message: 'Pet não encontrado !'})
+        return;
+    }
+
+    try {
+        await Pet.deleteOne({id: id})
+        
+        res.status(200).json({message: 'Pet removido com sucesso !'})
+
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
+});
 
 // exportando !
 module.exports = routes
