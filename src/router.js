180231169cb5e19817151bc8a675558365309cad
carloskauan <carloskauan190@gmail.com>
2022-04-17 16:19:23 -0300

    comentando métodos !

--- src/router.js
+++ src/router.js
@@ -4,20 +4,22 @@ const routes = express.Router()
 // banco fake
 let pets = [];
 
+// post {pet}
 routes.post('/pets', (req, res) => {
-    const { id, nome, dono, raca } = req.body;
-    const pet = { id, nome, dono, raca };
+    const { id, nome, dono, raca, idade } = req.body;
+    const pet = { id, nome, dono, raca, idade };
     pets.push(pet);
     
     return res.status(201).json(pet);
 });
 
+// get {pets}
 routes.get('/pets', (req, res) => { 
     const buscarPets = pets;
     
     return res.status(200).json(buscarPets);
 });
-
+// get {pets:id}
 routes.get('/pets/:pets_id', (req, res) => { 
     const { pets_id } = req.params;
     const pet = pets.find((pet) => pet.id == pets_id);
@@ -26,6 +28,7 @@ routes.get('/pets/:pets_id', (req, res) => {
     return res.status(200).json(pet);
 });
 
+// delete {pet}
 routes.delete('/pets/:pets_id', (req, res) => {
     const { pets_id } = req.params;
     const FiltrarPets = pets.filter((pet) => pet.id != pets_id);
@@ -33,7 +36,7 @@ routes.delete('/pets/:pets_id', (req, res) => {
 
     return res.status(204).json('foi deletado, meu nobre !');
 });
-
+// patch {pet:id}
 routes.patch('/pets/:pets_id', (req, res) => {
     const { nome, dono, raca } = req.body;
     const { pets_id } = req.params;
@@ -46,4 +49,5 @@ routes.patch('/pets/:pets_id', (req, res) => {
     return res.status(200).json(pet);
 })
 
+// exportando !
 module.exports = routes
